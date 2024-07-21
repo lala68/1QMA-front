858bbfa945ad2e10465c553cd46b0bf45a25ec5f
ladan <l.fejan@gmail.com>
2024-07-21 16:30:25 +0330

    add score details in result page

--- src/app/services/general/general.service.ts
+++ src/app/services/general/general.service.ts
@@ -13,11 +13,11 @@ export class GeneralService implements OnInit {
   userId: any;
   hasCompletedSignup: any;
   initData: any;
-  userObj: any = {};
+  userObj: any;
   clientInit: any;
   cities: any;
   token: any;
-  currentRout: any = '';
+  currentRout: any;
   startingGame: any = false;
   socket: any;
   players: any = [];
