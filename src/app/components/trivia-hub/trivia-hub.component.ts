cb3c00371533471b185a75bac56e0dc5dc338b0d
ladan <l.fejan@gmail.com>
2024-07-31 12:01:49 +0330

    fix response of questions in library

--- src/app/components/trivia-hub/trivia-hub.component.ts
+++ src/app/components/trivia-hub/trivia-hub.component.ts
@@ -4,22 +4,42 @@ import {SharedModule} from "../../shared/shared.module";
 import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import {GamesService} from "../../services/games/games.service";
+import {ClientService} from "../../services/client/client.service";
+import {TranslateModule} from "@ngx-translate/core";
+import {GeneralService} from "../../services/general/general.service";
+import {ConfigService} from "../../services/config/config.service";
 
 @Component({
   selector: 'app-trivia-hub',
   standalone: true,
-  imports: [CommonModule, SharedModule, FormsModule, RouterModule],
+  imports: [CommonModule, SharedModule, FormsModule, RouterModule, TranslateModule],
   templateUrl: './trivia-hub.component.html',
   styleUrl: './trivia-hub.component.scss'
 })
 export class TriviaHubComponent implements OnInit {
-  loading: boolean = true;
+  loading: boolean = false;
+  loadingContent: boolean = true;
+  selectedTabIndex: any = 0;
+  library: any = [];
+  search: any = '';
 
-  constructor(private gameService: GamesService) {
+  constructor(private gameService: GamesService, private clientService: ClientService,
+              public generalService: GeneralService, public configService: ConfigService) {
   }
 
   async ngOnInit(): Promise<any> {
-
+    if (!this.search || this.search.length > 2) {
+      this.library = [];
+      this.loadingContent = true;
+      this.clientService.getUserQuestions('', this.selectedTabIndex == 0
+        ? 'private'
+        : this.selectedTabIndex == 1
+          ? 'public'
+          : 'bookmarked', this.search).then(data => {
+        this.loadingContent = false;
+        this.library = data.data;
+      });
+    }
   }
 
 }
