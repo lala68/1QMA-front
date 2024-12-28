2ece686e3148d77e2d09a732d0438a007232d6dd
navid hero <navid_hero_1st@yahoo.com>
2024-12-28 10:46:10 +0330

    fix: base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // baseUrl: 'https://api.staging.1qma.games/',
-  baseUrl: 'https://api.1qma.games/',
+  baseUrl: 'https://api.staging.1qma.games/',
+  // baseUrl: 'https://api.1qma.games/',
   version: '1.0.37',
 };
